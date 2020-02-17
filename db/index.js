b883b03c262f0a521a64a3106df3a250ed8f4435
gmsantos <gabriel.ms1@hotmail.com>
2020-02-17 15:23:33 -0300

    Run query from index

--- db/index.js
+++ db/index.js
@@ -3,11 +3,12 @@ var pgp = require('pg-promise')()
 
 const username = process.env.RDS_USERNAME || 'twuser'
 const password = process.env.RDS_PASSWORD || 'mudar123'
-const host = process.env.RDS_HOST || 'localhost'
-const port = process.env.RDS_PORT || '5463'
+const host = process.env.RDS_HOST || 'tw-postgres-rds.c6kdngmld0xo.us-east-1.rds.amazonaws.com'
+const port = process.env.RDS_PORT || '5432'
 const database = process.env.RDS_DATABASE || 'mydatabase'
 
 var connectionString = process.env.RDS_DSN_URL || `postgres://${username}:${password}@${host}:${port}/${database}`
+console.log(connectionString)
 
 var db = pgp(connectionString)
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: process.env.TITLE || 'TreinaWeb' });
+  console.log('batata')
+  db.one('SELECT 1 AS value')
+    .then(function (data) {
+      console.log('entrou')
+      console.log(data)
+      res.render('index', { title: data.value})
+    })
+    .catch(function (error) {
+      console.log('erro')
+      console.error(error)
+      res.render('index', { error: error.value})
+    })
 });
 
 module.exports = router;
